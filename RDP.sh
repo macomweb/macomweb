a1e8cea5a69f082b6cb4110a604ff02ff5462ec9
macomweb <135864500+macomweb@users.noreply.github.com>
2023-06-15 21:26:23 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,10 +1,9 @@
- 
 #! /bin/bash
-printf "Installing RDP Be Patience... " >&2
+printf "Installing RDP Be Patience..." >&2
 {
-sudo useradd -m macomweb
-sudo adduser macomweb sudo
-echo 'macomweb:8426' | sudo chpasswd
+sudo useradd -m Realityinred
+sudo adduser realityinred sudo
+echo 'realityinred:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -12,24 +11,23 @@ sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y
-sudo adduser macomweb chrome-remote-desktop
+sudo apt install nautilus nano -y 
+sudo apt -y install obs-studio
+sudo apt -y install firefox
+sudo apt -y install qbittorrent
+sudo apt install nload
+sudo adduser realityinred chrome-remote-desktop
 } &> /dev/null &&
-printf "\nSetup Complete " >&2 ||
+printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - macomweb -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
-if sudo apt-get upgrade &> /dev/null
-then
-    printf "\n\nUpgrade Completed " >&2
-else
-    printf "\n\nError Occured " >&2
-fi
+su - realityinred -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword is: 1234 \n'
