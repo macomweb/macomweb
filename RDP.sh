d23bb88a10a8b45494a683ef2ce67e26da5d4bf7
macomweb <135864500+macomweb@users.noreply.github.com>
2023-06-18 22:29:59 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,55 +1,51 @@
 #!/bin/bash
 
-echo "Installing RDP. Please be patient..."
+printf "Installing RDP. Please be patient...\n" >&2
 
 # Create a new user.
-useradd -m macomweb
-adduser macomweb sudo
-echo "macomweb:8426" | chpasswd
+sudo useradd -m macomweb
+sudo adduser macomweb sudo
+echo 'macomweb:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 
-# Install Chrome Remote Desktop.
-apt-get update
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-dpkg -i chrome-remote-desktop_current_amd64.deb
-apt-get install -y --fix-broken
+# Install Debian package for NoMachine.
+wget https://download.nomachine.com/download/7.8/Linux/nomachine_7.8.1_1_amd64.deb -O nomachine.deb
+sudo dpkg -i nomachine.deb
+sudo apt-get install -y --fix-broken
 
 # Install Xfce4 desktop environment.
-DEBIAN_FRONTEND=noninteractive apt-get install -y xfce4 desktop-base
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y xfce4 desktop-base
 
-# Set up the Chrome Remote Desktop session.
-echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
-apt-get install -y xscreensaver
-systemctl disable lightdm.service
+# Set up the NoMachine session.
+echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/nxserver/custom.sessions
 
 # Install Google Chrome.
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-apt-get install -y --fix-broken
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+sudo dpkg -i chrome.deb
+sudo apt-get install -y --fix-broken
 
 # Install Nautilus and nano.
-apt-get install -y nautilus nano
+sudo apt-get install -y nautilus nano
 
-# Install NoMachine server.
-wget https://download.nomachine.com/download/7.8/Linux/nomachine_7.8.1_1_amd64.deb -O nomachine.deb
-dpkg -i nomachine.deb
-apt-get install -y --fix-broken
-
-# Create the nomachine group if it does not exist.
-groupadd nomachine
-
-# Add the user to the nomachine group.
-adduser macomweb nomachine
+# Add the user to the NoMachine group.
+sudo groupadd nomachine
+sudo adduser macomweb nomachine
 
 # Restart the NoMachine service.
-systemctl restart nxserver.service
+sudo systemctl restart nxserver.service
 
-# Get the IP address of the instance.
-ip_address=$(curl -s ifconfig.me)
+# Copy the Debian Linux command from https://remotedesktop.google.com/headless
+printf "\nCheck https://remotedesktop.google.com/headless\n"
+printf "Copy the Command Of Debian Linux and paste it here:\n"
+read -p "Paste Here: " CRP
+sudo su - macomweb -c """$CRP"""
+
+printf "\nSetup Complete\n"
 
 # Print the NoMachine connection information.
-echo "NoMachine connection information:"
-echo "  IP address: $ip_address"
-echo "  Port: 4000"
-echo "  Username: macomweb"
-echo "  Password: 8426"
+ip_address=$(curl -s ifconfig.me)
+printf "NoMachine connection information:\n"
+printf "  IP address: $ip_address\n"
+printf "  Port: 4000\n"
+printf "  Username: macomweb\n"
+printf "  Password: 8426\n"
