a52cf3a6fa1dce33fd5fd0cd9682d1b0a316f4a0
macomweb <135864500+macomweb@users.noreply.github.com>
2023-06-18 22:19:55 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,49 +1,48 @@
 #!/bin/bash
 
-# Installs Chrome Remote Desktop and NoMachine on a Linux machine.
-
-echo "Installing RDP Be Patience..."
+echo "Installing RDP. Please be patient..."
 
 # Create a new user.
 useradd -m macomweb
 adduser macomweb sudo
-echo "macomweb:8426" | sudo chpasswd
+echo "macomweb:8426" | chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 
 # Install Chrome Remote Desktop.
 apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg -i chrome-remote-desktop_current_amd64.deb
-apt install --assume-yes --fix-broken
+dpkg -i chrome-remote-desktop_current_amd64.deb
+apt-get install -y --fix-broken
 
 # Install Xfce4 desktop environment.
-DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base
+DEBIAN_FRONTEND=noninteractive apt-get install -y xfce4 desktop-base
 
 # Set up the Chrome Remote Desktop session.
 echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
-apt install --assume-yes xscreensaver
+apt-get install -y xscreensaver
 systemctl disable lightdm.service
 
 # Install Google Chrome.
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-apt install --assume-yes --fix-broken
+dpkg -i google-chrome-stable_current_amd64.deb
+apt-get install -y --fix-broken
 
 # Install Nautilus and nano.
-apt install nautilus nano -y
+apt-get install -y nautilus nano
 
 # Install NoMachine server.
-wget https://download.nomachine.com/free/linux/64/deb -O nomachine.deb
-sudo dpkg -i nomachine.deb
+wget https://download.nomachine.com/download/7.8/Linux/nomachine_7.8.1_1_amd64.deb -O nomachine.deb
+dpkg -i nomachine.deb
+apt-get install -y --fix-broken
 
 # Create the nomachine group if it does not exist.
-sudo groupadd nomachine
+groupadd nomachine
 
 # Add the user to the nomachine group.
-sudo adduser macomweb nomachine
+adduser macomweb nomachine
 
 # Restart the NoMachine service.
-sudo systemctl restart nomachine.service
+systemctl restart nxserver.service
 
 # Get the IP address of the instance.
 ip_address=$(curl -s ifconfig.me)
