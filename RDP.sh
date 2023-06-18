d9ecefbc581ea49f058c7a9b94f3b813a74cfec4
macomweb <135864500+macomweb@users.noreply.github.com>
2023-06-18 23:51:44 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,36 +1,47 @@
-#!/bin/bash
+wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
 
-echo "Installing RDP. Please wait..."
 
-# Create a new user.
-useradd -m macomweb
-adduser macomweb sudo
-echo "macomweb:8426" | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+function goto
+{
+    label=$1
+    cd 
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
 
-# Install Xfce4 desktop environment.
-DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base
+: ngrok
+clear
+echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP 
 
-# Download and install NoMachine.
-wget https://download.nomachine.com/6.12/Linux/nomachine_6.12.3_1_amd64.deb -O nomachine.deb
-sudo apt-get install -y gdebi
-sudo gdebi --non-interactive nomachine.deb
-
-# Create the nomachine group if it does not exist.
-sudo groupadd nomachine
-
-# Add the user to the nomachine group.
-sudo adduser macomweb nomachine
-
-# Restart the NoMachine service.
-sudo systemctl restart nomachine.service
-
-# Get the IP address of the instance.
-ip_address=$(curl -s ifconfig.me)
-
-# Print the NoMachine connection information.
-echo "NoMachine connection information:"
-echo "  IP address: $ip_address"
-echo "  Port: 4000"
-echo "  Username: macomweb"
-echo "  Password: 8426"
+clear
+echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
+echo "======================="
+echo "choose ngrok region (for better connection)."
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 4000 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=reality --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+clear
+echo "NoMachine: https://www.nomachine.com/download"
+echo Done! NoMachine Information:
+echo IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+echo User: reality
+echo Passwd: 123456
+echo "VM can't connect? Restart Cloud Shell then Re-run script."
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
